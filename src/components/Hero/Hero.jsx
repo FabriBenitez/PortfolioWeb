4df2e11db52646c0f72fdc813ce331d6018cc3f8
Fabrielmasme <119084090+Fabrielmasme@users.noreply.github.com>
2026-02-10 02:41:01 -0300

    Se configura la primer carpeta

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,12 @@
+import "./Hero.css";
+
+export default function Hero() {
+  return (
+    <section className="hero">
+      <div className="container">
+        <div className="left">Avatar</div>
+        <div className="right">Texto</div>
+      </div>
+    </section>
+  );
+}
